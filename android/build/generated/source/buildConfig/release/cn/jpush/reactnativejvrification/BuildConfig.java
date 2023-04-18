320b8f930ccbd62281b52242e4fe0988219df03c
jon <jon.sun@letote.cn>
2023-04-18 12:54:55 +0800

    upgrade jverification and change UI

---
+++ android/build/generated/source/buildConfig/release/cn/jpush/reactnativejvrification/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package cn.jpush.reactnativejvrification;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String LIBRARY_PACKAGE_NAME = "cn.jpush.reactnativejvrification";
+  public static final String BUILD_TYPE = "release";
+}
